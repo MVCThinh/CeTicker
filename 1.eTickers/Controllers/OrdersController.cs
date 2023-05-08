b63a1c7d18734daa00d141887aa82dad3117f614
HoangThinh <thinhhoang98@gmail.com>
2023-05-08 09:41:50 +0700

    Finish

--- 1.eTickers/Controllers/OrdersController.cs
+++ 1.eTickers/Controllers/OrdersController.cs
@@ -46,5 +46,41 @@ namespace _1.eTickers.Controllers
             return View(response);
         }
 
+        public async Task<IActionResult> AddItemToShoppingCart(int id)
+        {
+            var item = await _movieService.GetMovieByIdAsync(id);
+
+            if (item != null)
+            {
+                _shoppingCart.AddItemToCart(item);
+            }
+
+            return RedirectToAction(nameof(ShoppingCart));
+        }
+
+        public async Task<IActionResult> RemoveItemFromShoppingCart(int id)
+        {
+            var item = await _movieService.GetMovieByIdAsync(id);
+
+            if (item != null)
+            {
+                _shoppingCart.RemoveItemFromCart(item);
+            }
+
+            return RedirectToAction(nameof(ShoppingCart));
+        }
+
+        public async Task<IActionResult> CompleteOrder()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            string userEmailAddress = User.FindFirstValue(ClaimTypes.Email);
+
+            await _ordersService.StoreOrderAsync(items, userId, userEmailAddress);
+            await _shoppingCart.ClearShoppingCartAsync();
+
+            return View("OrderCompleted");
+        }
+
     }
 }
--- 1.eTickers/Data/ViewComponents/ShoppingCartSummary.cs
+++ 1.eTickers/Data/ViewComponents/ShoppingCartSummary.cs
@@ -1,7 +1,22 @@
-﻿namespace _1.eTickers.Data.ViewComponents
-{
-    public class ShoppingCartSummary
-    {
+﻿using _1.eTickers.Data.Cart;
+using Microsoft.AspNetCore.Mvc;
 
+namespace _1.eTickers.Data.ViewComponents
+{
+    public class ShoppingCartSummary :ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+
+            return View(items.Count);
+        }
     }
 }
