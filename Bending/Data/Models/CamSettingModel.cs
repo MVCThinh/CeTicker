a8e194d78ccdebccaff6adf67de2b57db57484fc
HoangThinh <thinhhoang98@gmail.com>
2023-05-11 17:22:54 +0700

    Thêm Model Setting

---
+++ Bending/Data/Models/CamSettingModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bending.Data.Models
+{
+    public class CamSettingModel
+    {
+        public string FOVX { get; set; }
+        public string FOVY { get; set; }
+        public string Resolution { get; set; }
+        public string Serial { get; set; }
+        public string PatternSearchMode { get; set; }
+        public string PatternSearchTool { get; set; }
+        public string CalType { get; set; }
+        public string LightType { get; set; }
+        public string LighComport { get; set; }
+        public string LightChanel { get; set; }
+        public string CameraReverse { get; set; }
+        public string ImageSaveType { get; set; }
+        public string GrapDelay { get; set; }
+        public string LightDelay { get; set; }
+        public string AlignLimitX { get; set; }
+        public string AlignLimitY { get; set; }
+        public string AlignLimitT { get; set; }
+        public string RetryLimitCount { get; set; }
+        public string RetryCaptureCount { get; set; }
+        public string ImageAutoDeleteDay { get; set; }
+        public string CenterAlign { get; set; }
+    }
+
+}
--- Bending/Data/Models/LaserModel.cs
+++ Bending/Data/Models/LaserModel.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Bending.Data.Models
 {
-    internal class LaserModel
+    public class LaserModel
     {
+        public string MarkPositionTolX { get; set; }
+        public string MarkPositionTolY { get; set; }
+        public string MaxSizeX { get; set; }
+        public string MaxSizeY { get; set; }
+        public string AlignRefSearch { get; set; }
+        public string UseImageProcessing { get; set; }
+        public string BlobSearchMaxPos { get; set; }
+        public string BlobBoxUsePoint { get; set; }
+        public string BlobPolarity { get; set; }
+        public string ConnectivityMinPixels { get; set; }
+        public string MCRSearch { get; set; }
+        public string MCRRight { get; set; }
+        public string MCRUp { get; set; }
+        public string InspectDegreeKind { get; set; }
+        public string LaserAlignRefPosTol { get; set; }
     }
 }
