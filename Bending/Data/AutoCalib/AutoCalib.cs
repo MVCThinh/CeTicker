8245709ec9d9ef08e87b1efb7aadd47340817022
HoangThinh <thinhhoang98@gmail.com>
2023-05-18 17:33:57 +0700

    1

--- Bending/Data/AutoCalib/AutoCalib.cs
+++ Bending/Data/AutoCalib/AutoCalib.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace Bending.Data.AutoCalib
 {
-    internal class AutoCalib
+    public class AutoCalib
     {
+        public static PointWithTheta[] listPointRB = new PointWithTheta[11];
+        public static PointWithTheta[] listPointCam = new PointWithTheta[11];
+
+
+
+
+
+        private void Calib9Point()
+        {
+
+        }
+
     }
+
+    public class PointWithTheta
+    {
+        public PointWithTheta(float pointX, float pointY, float pointTheta)
+        {
+            this.pointX = pointX;
+            this.pointY = pointY;
+            this.pointTheta = pointTheta;
+        }
+
+        public float pointX { get; set; }
+        public float pointY { get; set; }
+        public float pointTheta { get; set; }
+    }
+
 }
