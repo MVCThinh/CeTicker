7e9a2714658a4a15b5b2e8e65d791b2879e170cf
MVCThinh <thinhhoang98@gmail.com>
2023-06-09 17:31:20 +0700

    #

--- Bending/Data/PLC/PLCLoad.cs
+++ Bending/Data/PLC/PLCLoad.cs
@@ -9,5 +9,25 @@ namespace Bending.Data.PLC
     public class PLCLoad
     {
 
+
+        public IList<IList<int>> KSmallestPairs(int[] nums1, int[] nums2, int k)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+
+            if (nums1 == null || nums2 == null || nums1.Length == 0 || nums2.Length == 0)
+            {
+                return result;
+            }
+
+            PriorityQueue
+
+        }
+
+    }
+
+    // Class define priority queue
+    public class PriorityQueue<T> where T : IComparable<T>
+    {
+
     }
 }
